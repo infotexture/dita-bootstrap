c50f6ebe7de82ada28b56d3c21b6c8fdd0b5f1e5
GitHub Action <action@github.com>
2023-05-07 18:34:59 +0000

    🚀 Deploy infotexture/dita-bootstrap@631d531 to GitHub Pages

--- js/lunr-client.js
+++ js/lunr-client.js
@@ -1,13 +1,28 @@
 let LUNR_DATA = null;
 let PREVIEW_LOOKUP = null;
-const BASE_URL = window.location.origin + "/";
+const scripts = document.getElementsByTagName("script");
+const scriptPath = scripts[scripts.length - 1].src;
+const JSON_PATH = scriptPath.substr(0, scriptPath.lastIndexOf("/") + 1);
+const BASE_URL = scriptPath.substr(
+  0,
+  JSON_PATH.lastIndexOf("/", JSON_PATH.lastIndexOf("/") - 1) + 1
+);
 
-function jsonFetch(url) {
+function jsonFetch(json, filename) {
   return new Promise(function (resolve, reject) {
-    fetch(BASE_URL + url)
-      .then((response) => response.json())
-      .then((result) => {
-        resolve(result);
+    if (json) {
+      return resolve(json);
+    }
+
+    fetch(JSON_PATH + filename)
+      .then((response) => {
+        if (!response.ok) {
+          return reject(new Error(`HTTP error, status = ${response.status}`));
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return resolve(data);
       });
   });
 }
@@ -40,14 +55,14 @@ function parseLunrResults(results) {
 
 function formatResults(results) {
   return `<article role="article">
-		<div class="modal-header">
+    <div class="modal-header">
 	        <h1>Search Results</h1>
-	        <button type="button" class="btn-close" aria-label="Close" onclick="closeSearch(this);"></button>
-	    </div>
-	    <div class="modal-body">
-		${parseLunrResults(results)}
-		</div>
-		</article>`;
+          <button type="button" class="btn-close" aria-label="Close" onclick="closeSearch(this);"></button>
+      </div>
+      <div class="modal-body">
+    ${parseLunrResults(results)}
+    </div>
+    </article>`;
 }
 
 function escapeHtml(unsafe) {
@@ -66,31 +81,35 @@ function closeSearch(el) {
   return false;
 }
 
-async function search(el) {
-  if (!LUNR_DATA) {
-    LUNR_DATA = await jsonFetch("js/search_index.json");
-  }
+function search(el) {
+  Promise.all([
+    jsonFetch(LUNR_DATA, "search_index.json"),
+    jsonFetch(PREVIEW_LOOKUP, "preview.json"),
+  ])
+    .then((values) => {
+      LUNR_DATA = values[0];
+      PREVIEW_LOOKUP = values[1];
+      const query = document.getElementById("search-input").value;
+      const idx = lunr.Index.load(LUNR_DATA);
+      // Write results to page
+      const results = idx.search(query);
+      const elements = document.getElementsByClassName("bs-main");
 
-  if (!PREVIEW_LOOKUP) {
-    PREVIEW_LOOKUP = await jsonFetch("js/preview.json");
-  }
+      if (elements.length < 2) {
+        const resultHtml = `<main role="main" class="container bs-main">
+          ${formatResults(results)}</main>`;
 
-  const query = document.getElementById("search-input").value;
-  const idx = lunr.Index.load(LUNR_DATA);
-  // Write results to page
-  const results = idx.search(query);
-  const elements = document.getElementsByClassName("bs-main");
+        elements[0].insertAdjacentHTML("afterend", resultHtml);
+      } else {
+        elements[1].innerHTML = formatResults(results);
+      }
 
-  if (elements.length < 2) {
-    const resultHtml = `<main role="main" class="container bs-main">
-	    	${formatResults(results)}</main>`;
-
-    elements[0].insertAdjacentHTML("afterend", resultHtml);
-  } else {
-    elements[1].innerHTML = formatResults(results);
-  }
-
-  elements[0].classList.add("collapse");
-  window.scrollTo(0, 0);
+      elements[0].classList.add("collapse");
+      window.scrollTo(0, 0);
+      return false;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
   return false;
 }
