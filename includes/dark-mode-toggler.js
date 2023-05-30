01406c9a57d20b37a64348445056398c7552af1f
Jason Fox <jason-fox@users.noreply.github.com>
2023-05-30 18:46:39 +0200

    Update toggler

--- includes/dark-mode-toggler.js
+++ includes/dark-mode-toggler.js
@@ -5,70 +5,70 @@
  */
 
 (() => {
-  'use strict'
+  'use strict';
 
-  const storedTheme = localStorage.getItem('theme')
+  const getStoredTheme = () => localStorage.getItem('theme');
+  const setStoredTheme = theme => localStorage.setItem('theme', theme);
 
   const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
     if (storedTheme) {
-      return storedTheme
+      return storedTheme;
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  };
 
-  const setTheme = function (theme) {
+  const setTheme = theme => {
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark')
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', theme);
     }
-  }
+  };
 
-  setTheme(getPreferredTheme())
+  setTheme(getPreferredTheme());
 
-  const showActiveTheme = theme => {
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    try {
-      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+  const showActiveTheme = (theme, focus = false) => {
+    const themeSwitcher = document.querySelector('#bd-theme');
 
-      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-        element.classList.remove('active')
-      })
-
-      btnToActive.classList.add('active')
-      if (activeThemeIcon !== null) {
-        activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-      }
-    } catch {
-      // Using different theme names. Not just dark and light.
+    if (!themeSwitcher) {
+      return;
     }
-  }
+
+    const activeThemeIcon = document.querySelector('.theme-icon-active use');
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+      element.classList.remove('active');
+      element.setAttribute('aria-pressed', 'false');
+    });
+
+    btnToActive.classList.add('active');
+    btnToActive.setAttribute('aria-pressed', 'true');
+
+    if (focus) {
+      themeSwitcher.focus();
+    }
+  };
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    if (storedTheme !== 'light' || storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
+    const storedTheme = getStoredTheme();
+    if (storedTheme !== 'light' && storedTheme !== 'dark') {
+      setTheme(getPreferredTheme());
     }
-  })
+  });
 
-  window.addEventListener('load', () => {
-    showActiveTheme(getPreferredTheme())
+  window.addEventListener('DOMContentLoaded', () => {
+    showActiveTheme(getPreferredTheme());
 
-    document.querySelectorAll('[data-bs-theme-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
-          localStorage.setItem('theme', theme)
-          setTheme(theme)
-          showActiveTheme(theme)
-
-          // After changing the theme, hide offcanvas
-          const oc = document.querySelector('[aria-modal][class*=show][class*=offcanvas-]')
-          if (oc !== null) {
-            bootstrap.Offcanvas.getInstance(oc).hide()
-          }
-        })
-      })
-  })
-})()
+    document.querySelectorAll('[data-bs-theme-value]').forEach(toggle => {
+      toggle.addEventListener('click', () => {
+        const theme = toggle.getAttribute('data-bs-theme-value');
+        setStoredTheme(theme);
+        setTheme(theme);
+        showActiveTheme(theme, true);
+      });
+    });
+  });
+})();
